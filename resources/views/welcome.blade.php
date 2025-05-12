3e0d85079a09f5eb6f066b158c8fe0914979f9e6
tsaacod <maitsaluthfiyyah09@gmail.com>
2025-05-12 15:25:36 +0700

    perubahan dikit layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -260,7 +260,7 @@
         <footer class="bg-white">
             <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
                 <div class="text-center">
-                    <p class="text-base text-gray-400">&copy; 2024 E-Posyandu. All rights reserved.</p>
+                    <p class="text-base text-gray-400">&copy; 2025 E-Posyandu. All rights reserved.</p>
                 </div>
             </div>
         </footer>
